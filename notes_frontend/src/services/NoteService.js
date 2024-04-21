c3c77726090ff4cfb60336b75978dba7208247ac
Nguyen <nguyenknkn76@gmail.com>
2024-04-21 10:39:22 +0700

    push into main

--- notes_frontend/src/services/NoteService.js
+++ notes_frontend/src/services/NoteService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/notes'
-
+const baseUrl = 'http://localhost:3001/api/notes'
+    
 const getAll = () => {
     const request = axios.get(baseUrl)
     const nonExisting = {
